e8b949602ed451fb23828fade9c343d1b7e69113
김연희 <yonee@ogqcorp.com>
2021-03-03 14:06:35 +0900

    setting

--- 1-vanilla/js/models/HistoryModel.js
+++ 1-vanilla/js/models/HistoryModel.js
@@ -6,12 +6,17 @@ export default {
   ],
 
   list() {
+    /*
+    * return this.data로 하지 않은 이유
+    * 서버에서 비동기로 데이터를 가지고 오는 경우와 쿠키로 데이터를 얻는 경우가 있기때문에
+    * 공통으로 사용하기 위해 비동기로 처리
+    * */
     return Promise.resolve(this.data)
   },
-  
+
   add(keyword = '') {
     keyword = keyword.trim()
-    if (!keyword) return 
+    if (!keyword) return
     if (this.data.some(item => item.keyword === keyword)) {
       this.remove(keyword)
     }
@@ -19,7 +24,7 @@ export default {
     const date = '12.31'
     this.data = [{keyword, date}, ...this.data]
   },
-  
+
   remove(keyword) {
     this.data = this.data.filter(item => item.keyword !== keyword)
   }
