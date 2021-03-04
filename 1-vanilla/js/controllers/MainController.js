a7e6eafe56bdce354dd64afbbdb5ee13464519b0
김연희 <cablekim1204@gmail.com>
2021-03-05 01:11:50 +0900

    검색폼 구현

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -3,7 +3,16 @@ const tag = '[MainController]'
 
 export default {
   init() {
-    console.log(tag, 'init()')
     FormView.setup(document.querySelector('form'))
+        .on('@submit', e => this.onSubmit(e.detail.input))
+        .on('@reset', e => this.onResetForm())
+  },
+
+  onSubmit(input) {
+    console.log(tag, 'onSubmit()', input)
+  },
+
+  onResetForm() {
+    console.log(tag, 'onResetForm()')
   }
 }
