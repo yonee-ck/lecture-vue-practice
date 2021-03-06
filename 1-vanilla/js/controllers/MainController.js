0f97436d065bd624dd584ab04fb5a77b16b103cd
김연희 <cablekim1204@gmail.com>
2021-03-06 14:54:56 +0900

    search api 연동, 검색 결과 구현

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from '../views/FormView.js';
+import ResultView from '../views/ResultView.js';
+import SearchModel from '../models/SearchModel.js'
 const tag = '[MainController]'
 
 export default {
@@ -6,13 +8,29 @@ export default {
     FormView.setup(document.querySelector('form'))
         .on('@submit', e => this.onSubmit(e.detail.input))
         .on('@reset', e => this.onResetForm())
+    ResultView.setup(document.querySelector('#search-result'))
+
   },
+
+  search(query) {
+    console.log(tag, 'search()', query)
+    // search api
+    SearchModel.list(query).then(data => {
+      this.onSearchResult(data)
+    })
+    this.onSearchResult([])
+  }
 
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
+    this.search(input)
   },
 
   onResetForm() {
     console.log(tag, 'onResetForm()')
+  },
+
+  onSearchResult(data) {
+    ResultView.render(data)
   }
 }
